183a2b2d3ece1c13392549333fba2df51274b8d2
MildogMiller <37893674+MildogMiller@users.noreply.github.com>
2023-05-21 10:47:56 +0200

    fixed env issue

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,23 +1,24 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
+//commented out production env code
 
-const SEPURL = process.env.SEPOLIA_URL;
-const SEPKEY = process.env.SEPOLIA_PRIVATE_KEY;
+// const SEPURL = process.env.SEPOLIA_URL;
+// const SEPKEY = process.env.SEPOLIA_PRIVATE_KEY;
 /** @type import('hardhat/config').HardhatUserConfig */
 //hardhat network built in no need to specify its details
 module.exports = {
   solidity: "0.8.18",
   defaultNetwork: "hardhat",
-  networks: {
-    sepolia: {
-      url: SEPURL,
-      accounts: [SEPKEY],
-      chainId: 11155111
-    }
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY
-  }
+  // networks: {
+  //   sepolia: {
+  //     url: SEPURL,
+  //     accounts: [SEPKEY],
+  //     chainId: 11155111
+  //   }
+  // },
+  // etherscan: {
+  //   apiKey: process.env.ETHERSCAN_API_KEY
+  // }
   
 };
